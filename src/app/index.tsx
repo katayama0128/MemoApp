006335b1cef7d0a02f66102c8d9770c2875d49f4
tk145 <196783508+katayama0128@users.noreply.github.com>
2025-06-03 16:56:01 +0900

    Complete MarkUp & Styling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,28 +1,8 @@
-import { View, StyleSheet } from 'react-native'
-import Header from '../components/Header'
-import MemoListItem from '../components/MemoListItem'
-import CircleButton from '../components/CircleButton'
+import { Redirect } from "expo-router"
 
-const Index = (): JSX.Element => {
-  return (
-    <View style={styles.container}>
 
-      <Header />
-      <View>
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
-      </View>
-      <CircleButton>+</CircleButton>
-    </View>
-  )
+const Index = () => {
+  return <Redirect href='auth/sign_up' />
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1, //画面全体に広げる設定
-    backgroundColor: '#ffffffff'
-  }
-})
 
 export default Index
---
+++ src/app/memo/list.tsx
@@ -0,0 +1,32 @@
+import { View, StyleSheet } from 'react-native'
+
+import Header from '../../components/Header'
+import MemoListItem from '../../components/MemoListItem'
+import CircleButton from '../../components/CircleButton'
+import Icon from '../../components/Icon'
+
+const Index = (): JSX.Element => {
+  return (
+    <View style={styles.container}>
+
+      <Header />
+      <View>
+        <MemoListItem />
+        <MemoListItem />
+        <MemoListItem />
+      </View>
+      <CircleButton>
+        <Icon name='plus' size={40} color='#ffffff' />
+      </CircleButton>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, //画面全体に広げる設定
+    backgroundColor: '#ffffffff'
+  }
+})
+
+export default Index
